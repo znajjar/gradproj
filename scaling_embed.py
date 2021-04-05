fix bug with scaling when there's only one pin in the image

scaling_embed.py
@@ -34,8 +34,8 @@ def preprocess():
     is_rounded = get_is_rounded(processed_pixels_og, processed_pixels)[np.in1d(processed_pixels, mapped_values)]
     is_rounded = is_rounded.astype(np.bool)
 
-    processed_pixels += iterations
     processed_pixels = processed_pixels.astype(np.uint8)
+    processed_pixels += iterations
 
 
 def fill_buffer():

shared.py
@@ -81,7 +81,7 @@ def get_header_and_body(image: np.ndarray, header_size=HEADER_SIZE) -> (np.ndarr
 
 def get_mapped_values(og_max, scaled_max):
     scale_factor = scaled_max / og_max
-    og_values = np.arange(MAX_PIXEL_VALUE)
+    og_values = np.arange(MAX_PIXEL_VALUE + 1)
 
     scaled_values = og_values * scale_factor
     scaled_values -= EPS