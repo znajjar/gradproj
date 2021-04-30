59b63d8c5c21cfa707404ce807d7d9016badaeab
Zeyad <zeyadnajjar999@gmail.com>
2021-04-30 19:15:03 +0300

    simplified reading images in stats

--- src/stats.py
+++ src/stats.py
@@ -1,6 +1,6 @@
 import os
 import traceback
-from os import path
+from os.path import join as join_path
 
 import cv2
 import numpy as np
@@ -11,32 +11,31 @@ from util.measure import Measure
 from util.util import bits_to_bytes, read_image, is_image
 from write_data import RunStats, ImageStats, write_data
 
-ORIGINAL_IMAGES_PATH = 'res/dataset-50/'
-original_images = [f'{i}.gif' for i in range(1, 50)]  # path relative to ORIGINAL_IMAGES_PATH
+IMAGES_PATH = 'res/dataset-50/'
+original_images = []  # path relative to ORIGINAL_IMAGES_PATH
 
+# if list is empty, find all images in ORIGINAL_IMAGES_PATH
 if not original_images:
-    for f in os.listdir(ORIGINAL_IMAGES_PATH):
-        joined_path = path.join(ORIGINAL_IMAGES_PATH, f)
+    for f in os.listdir(IMAGES_PATH):
+        joined_path = join_path(IMAGES_PATH, f)
         if is_image(joined_path):
-            original_images.append(joined_path)
-else:
-    original_images = [ORIGINAL_IMAGES_PATH + file for file in original_images]
+            original_images.append(f)
 
-print(len(original_images))
-
-original_images = [(os.path.split(image)[1], read_image(image)) for image in original_images]
+# read all images and pair them with their filenames
+original_images = [(image, read_image(join_path(IMAGES_PATH, image))) for image in original_images]
 
 RDH_ALGORITHMS = [
-    original_algorithm,
-    scaling_algorithm,
-    bp_scaling_algorithm,
+    # original_algorithm,
+    # scaling_algorithm,
+    # bp_scaling_algorithm,
     uni_algorithm,
-    bp_uni_algorithm_improved,
-    bp_uni_algorithm,
+    # bp_uni_algorithm_improved,
+    # bp_uni_algorithm,
 ]
 
 np.random.seed(2115)
 data = bits_to_bytes(np.random.randint(0, 2, size=2000 * 2000) > 0)
+
 for rdh_embedder, rdh_extractor, label in RDH_ALGORITHMS:
     stopwatch = Measure()
     print('======================')
@@ -54,7 +53,8 @@ for rdh_embedder, rdh_extractor, label in RDH_ALGORITHMS:
             print(f'{iterations_count} iterations:')
 
             try:
-                recovered_image, extraction_iterations, extracted_data = Measure(extractor.extract, print_time=True)(embedded_image)
+                recovered_image, extraction_iterations, extracted_data = Measure(extractor.extract, print_time=True)(
+                    embedded_image)
                 is_successful = \
                     not np.any(original_image - recovered_image) and extraction_iterations == iterations_count
                 hidden_data_size = len(extracted_data) * 8
